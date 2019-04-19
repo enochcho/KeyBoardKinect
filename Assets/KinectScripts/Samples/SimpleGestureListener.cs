0b25415f7f90497037ee2e6c44883bbbf173b6b9
celeste8 <celeste8@live.unc.edu>
2019-04-19 14:28:48 -0400

    New Scripts

--- Assets/KinectScripts/Samples/SimpleGestureListener.cs
+++ Assets/KinectScripts/Samples/SimpleGestureListener.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System;
 using UnityEngine.UI;
+using System.IO;
 
 public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListenerInterface
 {
@@ -11,26 +12,11 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
     // GUI Text to display the score.
     public Text scoreInfo;
 
-    // Keys
-    public GameObject b3;     //it keeps saying planet names because the tags on 
-    public GameObject c4;     //the keys are the planet's names...
-    public GameObject cs4;
-    public GameObject d4;
-    public GameObject ds4;
-    public GameObject e4;
-    public GameObject f4;
-    public GameObject fs4;
-    public GameObject g4;
-    public GameObject gs4;
-    public GameObject a4;
-    public GameObject as4;
-    public GameObject b4;
-    public GameObject c5;
-    public GameObject cs5;
-    public GameObject d5;
+    // Song to play
+    Song currentSong;
 
+    // GameObjects
     public GameObject startbutton;
-
     public GameObject spaceship;
 
     // private bool to track if progress message has been displayed
@@ -38,81 +24,40 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
 
     private bool keyClicked = false;
     public GameObject selectedKey;
-    //private correctlyPlacedScript correctlyPlaced;
-    static float dottedhalf = 1f;
-    static float quarter = .33f;
-    static float half = .66f;
-    static float tied = 2f;
-    static float whole = 1.33f;
 
-   // public int numNotesPlayed;
-   // public int level = 0;
     int numNotesPlayed = 0;
     int level = 0;
     bool endGame = false;
     int score = 0;
+    // Arrays for songs 
+    // Need a method to assign songs below to these arrays based on button clicked
+    Key[] player;
+    Key[] correct;
+    float[] duration;
+    int[] notesPerLevel;
+    int[] notesPerLevelSum;
 
-    float[] duration = new float[] {
-            half, quarter, quarter, quarter, quarter, dottedhalf, dottedhalf,
-            half, quarter, quarter, quarter, quarter, tied,
-            quarter, quarter, quarter, quarter, quarter, quarter,
-            half, quarter, dottedhalf,
-            half, quarter, quarter, quarter, quarter,
-            quarter, quarter, quarter, quarter, quarter, quarter,
-            half, quarter, quarter, quarter, quarter, dottedhalf, half, quarter,
-            half, quarter, quarter, quarter, quarter, whole,
-            quarter, quarter, dottedhalf, dottedhalf, quarter, quarter, quarter, quarter,
-            quarter, quarter, dottedhalf, dottedhalf, tied
-        };
-    GameObject[] player=new GameObject[60];
-    GameObject[] correct; 
-
-    int[] notesPerLevel = new int[]
-        {
-            7, 6, 6, 3, 5, 6, 8, 6, 8, 5, 60
-        };
-    int[] notesPerLevelSum = new int[]
+    void Start()
     {
-            0, 7, 13, 19, 22, 27, 33, 41, 47, 55, 60
-    };
 
-    void Awake() {
-        b3 = GameObject.Find("Key B3");
-        c4 = GameObject.Find("Key C4");
-        cs4 = GameObject.Find("Key C#4");
-        d4 = GameObject.Find("Key D4");
-        ds4 = GameObject.Find("Key D#4");
-        e4 = GameObject.Find("Key E4");
-        f4 = GameObject.Find("Key F4");
-        fs4 = GameObject.Find("Key F#4");
-        g4 = GameObject.Find("Key G4");
-        gs4 = GameObject.Find("Key G#4");
-        a4 = GameObject.Find("Key A4");
-        as4 = GameObject.Find("Key A#4");
-        b4 = GameObject.Find("Key B4");
-        c5 = GameObject.Find("Key C5");
-        cs5 = GameObject.Find("Key C#5");
-        d5 = GameObject.Find("Key D5");
         startbutton = GameObject.Find("Start");
 
-         correct = new GameObject[] {
-            c4, c5, a4, g4, e4, g4, d4,
-            c4, c5, a4, g4, e4, g4,
-            a4, gs4, a4, e4, f4, g4,
-            a4, f4, d4,
-            a4, a4, a4, b4, c5,
-            d5, b4, a4, g4, e4, d4,
-            c4, c5, a4, g4, e4, g4, d4, d4,
-            c4, d4, e4, f4, g4, a4,
-            a4, b4, c5, c5, c5, b4, a4, g4,
-            fs4, g4, a4, b4, c5
-        };
+        // Set currentSong based on what the player chooses
+        // Now defaults to Ballgame
+        currentSong = new Song(0);
+        // set all of the arrays here
+        player = new Key[currentSong.getNumNotes()];
+        correct = currentSong.getSongNotes();
+        duration = currentSong.getKeyDurations();
+        notesPerLevel = currentSong.getNotesPerLevel();
+        notesPerLevelSum = currentSong.getNotesPerLevelSum();
 
+        // set score text
         scoreInfo.text = "Score:";
 
+        // Maybe put this elsewhere? Start on play?
         StartCoroutine(PlayForTime(correct, duration, 0, notesPerLevel[0]));
     }
-
 
     public void UserDetected(uint userId, int userIndex)
     {
@@ -121,7 +66,7 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
 
         if (gestureInfo != null)
         {
-           //gestureInfo.text = "Capture planets by clicking on them!";
+            //gestureInfo.text = "Capture planets by clicking on them!";
         }
     }
 
@@ -141,9 +86,8 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
             string sGestureText = string.Format("capture {0:F1}% complete", progress * 100);
 
             if (gestureInfo != null)
-             //   gestureInfo.text = sGestureText;
-
-            progressDisplayed = true;
+                //   gestureInfo.text = sGestureText;
+                progressDisplayed = true;
         }
     }
 
@@ -182,9 +126,6 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
                     break;
             }
         }
-
-
-
         if (gestureInfo != null)
         {
             gestureInfo.text = sGestureText;
@@ -210,43 +151,42 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
         return true;
     }
 
-    public IEnumerator PlayForTime(GameObject[] notes, float[] durations, int start, int stop)
-    {   AudioSource audio1;
-        GameObject key;
+    public IEnumerator PlayForTime(Key[] notes, float[] durations, int start, int stop)
+    {
+        AudioSource audio1;
+        Key key;
         Color32 startColor;
         Color32 flashColor = Color.cyan;
-        for(int i = start; i<stop; i++){
+        for (int i = start; i < stop; i++)
+        {
             key = notes[i];
             audio1 = notes[i].GetComponent<AudioSource>();
-            //audio1.time = audio1.clip.length -
             audio1.Play();
-            //audio1.SetScheduledEndTime(t0 + duration[i]);
             startColor = key.GetComponent<SpriteRenderer>().color;
             key.GetComponent<SpriteRenderer>().color = flashColor;
             yield return new WaitForSeconds(durations[i]);
-            key.GetComponent<SpriteRenderer>().color = startColor; 
-            //Invoke("StopAudio",durations[i]);
+            key.GetComponent<SpriteRenderer>().color = startColor;
         }
     }
 
 
-    public IEnumerator PlayBack(GameObject[] notes, float[] durations, int start, int stop)
+    public IEnumerator PlayBack(Key[] notes, float[] durations, int start, int stop)
     {
         AudioSource audio1;
-        GameObject key;
+        Key key;
         Color32 startColor;
         Color32 flashColor = Color.green;
-        
+
         for (int i = start; i < stop; i++)
         {
             key = notes[i];
             Debug.Log(key.name);
             Debug.Log(correct[i].name);
-            if (String.Compare(key.name,correct[i].name)!=0)
+            if (String.Compare(key.name, correct[i].name) != 0)
             {
                 flashColor = Color.red;
             }
-            if (String.Compare(key.name,correct[i].name)==0)
+            if (String.Compare(key.name, correct[i].name) == 0)
             {
                 flashColor = Color.green;
                 score += 100;
@@ -255,21 +195,13 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
             String scoreText = "Score: " + score.ToString();
             scoreInfo.text = scoreText;
             audio1 = notes[i].GetComponent<AudioSource>();
-            //audio1.time = audio1.clip.length -
             audio1.Play();
-            //audio1.SetScheduledEndTime(t0 + duration[i]);
             startColor = key.GetComponent<SpriteRenderer>().color;
             key.GetComponent<SpriteRenderer>().color = flashColor;
             yield return new WaitForSeconds(durations[i]);
             key.GetComponent<SpriteRenderer>().color = startColor;
-            //Invoke("StopAudio",durations[i]);
         }
     }
-
-
-
-
-
 
     /* private IEnumerator LightUpKey(GameObject key, float time)
      {
@@ -282,11 +214,10 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
          key.GetComponent<MeshRenderer>().material.color = startColor;
      }*/
 
-
-    // private void StopAudio()
-    // {
-    //     GetComponent<AudioSource>().Stop();
-    // }
+    /* private void StopAudio()
+     {
+         GetComponent<AudioSource>().Stop();
+     } */
 
     void Update()
     {
@@ -299,7 +230,7 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
             startbutton.GetComponent<SpriteRenderer>().color = Color.white;
             canMoveOn = true;
             playMore = false;
-            if (level == 9)
+            if (level == (notesPerLevel.Length - 1))
             {
                 endGame = true;
             }
@@ -324,7 +255,7 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
                     selectedKey = hit.transform.gameObject;
                     selectedKey.GetComponent<AudioSource>().Play();
                     numNotesPlayed++;
-                    player[notesPerLevelSum[level] + numNotesPlayed-1] = selectedKey;                
+                    player[notesPerLevelSum[level] + numNotesPlayed - 1] = selectedKey.GetComponent<Key>();
                 }
                 if (hit.transform.gameObject.name.Contains("Start") && canMoveOn)
                 {
@@ -333,20 +264,18 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
                         startbutton.GetComponent<SpriteRenderer>().color = Color.grey;
                         level++;
                         numNotesPlayed = 0;
-                        StartCoroutine(PlayForTime(correct, duration, notesPerLevelSum[level], notesPerLevelSum[level+1]));
+                        StartCoroutine(PlayForTime(correct, duration, notesPerLevelSum[level], notesPerLevelSum[level + 1]));
                     }
                     else
                     {
                         Debug.Log("end game");
+                        // For Practice => just move on
                         startbutton.GetComponent<SpriteRenderer>().color = Color.grey;
                         canMoveOn = false;
-                        StartCoroutine(PlayBack(player, duration, 0, 60));         //if player keeps track of the notes they've played, this should work 
-                        // tally up score and possibly update in real time
+                        StartCoroutine(PlayBack(player, duration, 0, 60));
                     }
                 }
             }
         }
     }
-        
-    }
-//}
+}
---
+++ Assets/Scripts/SetOfKeys.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Currently not in use, but good for modularity when we get the chance
+public class SetOfKeys : MonoBehaviour
+{
+    // Keys
+    public Key b3, c4, cs4, d4, ds4, e4, f4, fs4, g4, gs4, a4, as4, b4, c5, cs5, d5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        b3 = (Key)GameObject.Find("Key B3").GetComponent<Key>();
+        c4 = (Key)GameObject.Find("Key C4").GetComponent<Key>();
+        cs4 = (Key)GameObject.Find("Key C#4").GetComponent<Key>();
+        d4 = (Key)GameObject.Find("Key D4").GetComponent<Key>();
+        ds4 = (Key)GameObject.Find("Key D#4").GetComponent<Key>();
+        e4 = (Key)GameObject.Find("Key E4").GetComponent<Key>();
+        f4 = (Key)GameObject.Find("Key F4").GetComponent<Key>();
+        fs4 = (Key)GameObject.Find("Key F#4").GetComponent<Key>();
+        g4 = (Key)GameObject.Find("Key G4").GetComponent<Key>();
+        gs4 = (Key)GameObject.Find("Key G#4").GetComponent<Key>();
+        a4 = (Key)GameObject.Find("Key A4").GetComponent<Key>();
+        as4 = (Key)GameObject.Find("Key A#4").GetComponent<Key>();
+        b4 = (Key)GameObject.Find("Key B4").GetComponent<Key>();
+        c5 = (Key)GameObject.Find("Key C5").GetComponent<Key>();
+        cs5 = (Key)GameObject.Find("Key C#5").GetComponent<Key>();
+        d5 = (Key)GameObject.Find("Key D5").GetComponent<Key>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
